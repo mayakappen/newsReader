bf313ad5310b508fe1f243035b6f34f4461deec2
Maya Kappen <mayakappen@gmail.com>
2022-11-02 09:07:03 -0600

    Add fetched response to stories

--- src/App.js
+++ src/App.js
@@ -7,16 +7,30 @@ function App() {
   const [stories, setStories] = useState([])
   const [section, setSection] = useState('home')
   const [isLoading, setLoading] = useState(true)
-const articles = []
-  useEffect((articles) => {
-  articles = getTopStories(section)
- console.log(articles)
+
+
+useEffect(() => {
+ getTopStories(section)
+  .then(data => {
+    setStories(data)
+  })
+  console.log(stories)
+
  }, [])
+
+// const articles = stories.map(story =>
+//     <li>{story.title}</li>
+//   )
+
+// const displayStories = articles.results.map((result) => {
+//    return <li>{result.title}</li>
+//   })
+
   return (
     <section>
 
     </section>
   )
-}
+  }
 
 export default App;
