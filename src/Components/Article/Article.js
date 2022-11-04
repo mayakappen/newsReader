f6f54d76c962b7e4f847eff47e117c4fceffdb9a
Maya Kappen <mayakappen@gmail.com>
2022-11-04 01:31:46 -0600

    Add styling for all articles view

--- src/Components/Article/Article.js
+++ src/Components/Article/Article.js
@@ -12,6 +12,8 @@ function Article(props) {
         abstract: '',
         url: ''
     })
+    const [date, setDate] = useState('')
+
     useEffect(() => {
         const story = props.selected
         setStory({
@@ -25,16 +27,19 @@ function Article(props) {
             abstract: story.abstract,
             url: story.url
         })
+        const datee = props.getDate(story.published_date)
+        setDate(datee)
         console.log(story)
+        console.log(date)
     }, [])
     return (
         <div className="article-view" key={Date.now}>
            <h1>{story.title}</h1>
-           <h2>{story.byline}</h2>
+           <h4>{story.byline}</h4>
            <h3>{story.subsection}</h3>
-            <img src={story.multimedia[0].url}/>
-            <p>{story.published_date}</p>
-            <h4>{story.abstract}</h4>
+            <img src={story.multimedia[0].url} alt={story.multimedia[0].caption}/>
+            <p>{date}</p>
+            <p>{story.abstract}</p>
             <a href={story.url}>Full Story ➡️</a>
         </div>
     );
