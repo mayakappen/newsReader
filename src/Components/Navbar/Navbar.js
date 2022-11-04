da94f07d9fff4e889026c265974bdfc26edda6c6
Maya Kappen <mayakappen@gmail.com>
2022-11-03 21:51:00 -0600

    Add navbar styling

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,20 +1,24 @@
+import './Navbar.css'
+
 function Navbar(props) {
-const setSection = (section) => {
+
+    const setSection = (section) => {
     props.setSection(section)
 }
     return (
-        <nav>
-            <button onClick={() => setSection('us')}>US</button>
-            <button onClick={() => setSection('world')}>World</button>
-            <button onClick={() => setSection('nyregion')}>NY</button>
-            <button onClick={() => setSection('business')}>Business</button>
-            <button onClick={() => setSection('opinion')}>Opinion</button>
-            <button onClick={() => setSection('sports')}>Sports</button>
-            <button onClick={() => setSection('arts')}>Arts</button>
-            <button onClick={() => setSection('books')}>Books</button>
-            <button onClick={() => setSection('fashion')}>Style</button>
-            <button onClick={() => setSection('food')}>Food</button>
-            <button onClick={() => setSection('technology')}>Tech</button>
+        <nav className="navbar">
+            <h5 className="home" onClick={() => setSection('home')}>NewsReader</h5>
+            <button className="navBtn"onClick={() => setSection('us')}>US</button>
+            <button className="navBtn" onClick={() => setSection('world')}>World</button>
+            <button className="navBtn" onClick={() => setSection('nyregion')}>NY</button>
+            <button className="navBtn" onClick={() => setSection('business')}>Business</button>
+            <button className="navBtn" onClick={() => setSection('opinion')}>Opinion</button>
+            <button className="navBtn" onClick={() => setSection('sports')}>Sports</button>
+            <button className="navBtn" onClick={() => setSection('arts')}>Arts</button>
+            <button className="navBtn" onClick={() => setSection('books')}>Books</button>
+            <button className="navBtn" onClick={() => setSection('fashion')}>Style</button>
+            <button className="navBtn" onClick={() => setSection('food')}>Food</button>
+            <button className="navBtn" onClick={() => setSection('technology')}>Tech</button>
         </nav>
       );
 }
